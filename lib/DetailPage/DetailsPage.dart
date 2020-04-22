daa034e4c7ac9f44fff83d4efeded69cf9667859
gubewgibi <47559465+gubewgibi@users.noreply.github.com>
2020-04-22 17:17:58 +0700

    ทำให้เลื่อนได้
    ทำให้หน้าdetailsเลื่อนได้ แต่ยังmapก็ต้องเลื่อนลงด้วย

--- lib/DetailPage/DetailsPage.dart
+++ lib/DetailPage/DetailsPage.dart
@@ -54,101 +54,106 @@ class _DetailsPageState extends State<DetailsPage> with SingleTickerProviderStat
             )
           ],
         ),
-        body: ListView(scrollDirection: Axis.vertical
-            ,children: [
-          Stack(children: [
-            Container(
-                height: MediaQuery.of(context).size.height - 82.0,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.transparent),
-            Positioned(
-                top: 75.0,
-                child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(45.0),
-                          topRight: Radius.circular(45.0),
-                        ),
-                      color: Colors.orange,),
-                    height: MediaQuery.of(context).size.height - 100.0,
-                    width: MediaQuery.of(context).size.width)),
-            Positioned(
-                top: 30.0,
-                left: (MediaQuery.of(context).size.width / 2) - 100.0,
-                child: Hero(
-                    tag: widget.heroTag,
-                    child: Container(
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage(widget.heroTag),
-                                fit: BoxFit.cover)),
-                        height: 200.0,
-                        width: 200.0))),
-            Positioned(
-                top: 250.0,
-                left: 25.0,
-                right: 25.0,
+        body: ListView(
+            children: [
+              SingleChildScrollView(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(widget.foodName,
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 22.0,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold)),
-                    SizedBox(height: 20.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(widget.foodPrice,
-                            style: TextStyle(
-                                fontFamily: 'Montserrat',
-                                fontSize: 20.0,
-                                color: Colors.grey)),
-                        Container(height: 25.0, color: Colors.grey, width: 1.0),
-                      ],
-                    ),
-                    SizedBox(height: 20.0),
-                    Container(
-                        height: 100.0,
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: <Widget>[
-                            _buildInfoCard('Zone', widget.room, 'Lx'),
-                            SizedBox(width: 10.0),
-                            _buildInfoCard('Subject', widget.name, 'Subject'),
-                            SizedBox(width: 10.0),
-                          ],
-                        )
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 20),
-                      child: Text(widget.info),
-                      width: 400,
-                      height: 150,
-
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(bottom:5.0),
-                      child: Container(
-                        height: 100.0,
-                        child: Center(
-                          child: Text(
-                              widget.floor,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                  color: Colors.white,
+                    Stack(children: [
+                      Container(
+                          height: MediaQuery.of(context).size.height ,
+                          width: MediaQuery.of(context).size.width,
+                          color: Colors.transparent),
+                      Positioned(
+                          top: 75.0,
+                          child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(45.0),
+                                  topRight: Radius.circular(45.0),
+                                ),
+                                color: Colors.orange,),
+                              height: MediaQuery.of(context).size.height - 100.0,
+                              width: MediaQuery.of(context).size.width)),
+                      Positioned(
+                          top: 30.0,
+                          left: (MediaQuery.of(context).size.width / 2) - 100.0,
+                          child: Hero(
+                              tag: widget.heroTag,
+                              child: Container(
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: AssetImage(widget.heroTag),
+                                          fit: BoxFit.cover)),
+                                  height: 200.0,
+                                  width: 200.0))),
+                      Positioned(
+                          top: 250.0,
+                          left: 25.0,
+                          right: 25.0,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(widget.foodName,
+                                  style: TextStyle(
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 22.0,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold)),
+                              SizedBox(height: 20.0),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text(widget.foodPrice,
+                                      style: TextStyle(
+                                          fontFamily: 'Montserrat',
+                                          fontSize: 20.0,
+                                          color: Colors.grey)),
+                                  Container(height: 25.0, color: Colors.grey, width: 1.0),
+                                ],
+                              ),
+                              SizedBox(height: 20.0),
+                              Container(
+                                  height: 100.0,
+                                  child: ListView(
+                                    scrollDirection: Axis.horizontal,
+                                    children: <Widget>[
+                                      _buildInfoCard('Zone', widget.room, 'Lx'),
+                                      SizedBox(width: 10.0),
+                                      _buildInfoCard('Subject', widget.name, 'Subject'),
+                                      SizedBox(width: 10.0),
+                                    ],
+                                  )
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(top: 20),
+                                child: Text(widget.info),
+                                width: 400,
+                                height: 150,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(bottom:0.0),
+                                child: Container(
+                                  height: 50.0,
+                                  child: Center(
+                                    child: Text(
+                                        widget.floor,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        )
+                                    ),
+                                  ),
+                                ),
                               )
-                          ),
-                        ),
-                      ),
-                    )
+                            ],
+                          )),
+                      bottomSheet(img: widget.img,),
+                    ])
                   ],
-                )),
-            bottomSheet(img: widget.img,),
-          ])
+                ),
 
+              ),
         ]));
 
   }
